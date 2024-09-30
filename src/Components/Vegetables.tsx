da9b7c781c75e366c2347a1362bdff9d8f4faa68
Navin Kumaran <navinkumaran2004@gmail.com>
2024-09-30 21:13:26 +0530

    ...

--- src/Components/Vegetables.tsx
+++ src/Components/Vegetables.tsx
@@ -1,17 +1,16 @@
-import { Flex, Heading } from "@radix-ui/themes";
+import { Flex, Grid, Heading } from "@radix-ui/themes";
 import CardComp from "./CardComp";
 import { vegetables } from "../utils/consts";
-
 const Vegetables = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <Flex direction={"column"} gap={"6"} p={"6"}>
       <Heading className="bg-[#0D7C66] text-white max-w-fit px-4 py-1 rounded-2xl">Vegetables</Heading>
-      <div className="grid grid-cols-5 items-center justify-center gap-4">
+      <Flex wrap='wrap' gap="5">
         {vegetables.map((item) => (
           <CardComp name={item.name} image={item.image} count={item.count} />
         ))}
-      </div>
-    </div>
+      </Flex>
+    </Flex>
   );
 };
 
